6c90fa5d9747a940220dbc487ed5d4d729a7697a
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2021-12-02 15:52:14 +0100

    @here i slack-kanal ved alert, og bedre beskrivelse av feilen

--- src/proxyApi.js
+++ src/proxyApi.js
@@ -4,7 +4,7 @@ const securityUtils = require('./securityUtils.js')
 function setupProxy(app) {
     // Proxy endpoints
     app.use('/norg2', securityUtils.authenticateToken, createProxyMiddleware({
-        target: process.env.NNNORG2_BASE_URL,
+        target: process.env.NORG2_BASE_URL,
         changeOrigin: true,
         onProxyReq: (proxyReq => proxyReq.removeHeader('authorization')),
     }));
